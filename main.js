14877fc0352255c3851c35b7787f1a9f219697cd
miacarmen <maitreyacarmen@gmail.com>
2022-04-23 16:16:12 -0700

    added more starter files and pseudocode

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ class Profile {
 
     }
 }
+
+
+// WHEN I am prompted for my team members and their information
+
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated that displays a nicely formatted team roster based on user input
