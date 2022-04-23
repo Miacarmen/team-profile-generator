0da41e694328b9a1e3ddcabd4ed0c0b53a0be38c
miacarmen <maitreyacarmen@gmail.com>
2022-04-23 15:44:37 -0700

    added initial files and packages

---
+++ employee.js
@@ -0,0 +1,30 @@
+// Role classes
+
+class Employee {
+    constructor() {
+
+    }
+}
+
+
+class Manager {
+    constructor() {
+        
+    }
+}
+
+
+
+class Engineer {
+    constructor() {
+        
+    }
+}
+
+
+
+class Intern {
+    constructor() {
+        
+    }
+}
