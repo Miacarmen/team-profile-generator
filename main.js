775ee4c776f2d428fcf87b61e5531e2e20ed633d
miacarmen <maitreyacarmen@gmail.com>
2022-04-24 14:20:22 -0700

    added prompt questions and laid out main functions

--- main.js
+++ main.js
@@ -1,14 +1,67 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-class Profile {
-    constructor() {
-
-    }
-}
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./employee");
 
 // WHEN I am prompted for my team members and their information
 
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated that displays a nicely formatted team roster based on user input
+
+// get client input data
+// do something with that data
+
+inquirer
+.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "Enter employee's name:",
+  },
+
+  {
+    type: "number",
+    name: "id",
+    message: "Enter employee's ID:",
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "Enter employee's email:",
+  },
+
+  {
+      type: 'list',
+      name: 'role',
+      message: 'Select Employee\'s role from the list below',
+      choices: ['Manager', 'Engineer', 'Intern']
+  }
+  .then(response => {
+      console.log(response);
+      const data = employee(response);
+      writeToFile(data);
+  })
+]);
+
+// if epmloyee type === Manager
+// ask questions + manager questions
+
+// if employee type === Engineer
+// ask questions + engineer questions
+
+// if employee type === Intern
+// ask questions + intern questions
+
+function writeToFile(data) {
+  fs.writeFile("../dist/index.html", data, function (err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log("Success");
+    }
+  });
+}
+
+
+
+function generateHtml() {}
