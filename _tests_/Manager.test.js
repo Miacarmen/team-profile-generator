5087160a63df3e04e8d82fb532bd4c702d86f9de
miacarmen <maitreyacarmen@gmail.com>
2022-04-24 18:08:02 -0700

    styled HTML and CSS, required files, started filling out functions

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("@jest/globals");
+const { it } = require("jest-circus");
+const { describe } = require("yargs");
+const Manager = require("../lib/manager");
+
+describe("Manager class", () => {
+    it("Creates a new instance of Manager"), () => {
+        expect();
+    }
+});
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("./employee");
+const Employee = require("../lib/employee");
+
 
 // WHEN I am prompted for my team members and their information
 
@@ -8,10 +9,7 @@ const Employee = require("./employee");
 // THEN I exit the application, and the HTML is generated that displays a nicely formatted team roster based on user input
 
 // get client input data
-// do something with that data
-
-inquirer
-.prompt([
+inquirer.prompt([
   {
     type: "input",
     name: "name",
@@ -31,29 +29,30 @@ inquirer
   },
 
   {
-      type: 'list',
-      name: 'role',
-      message: 'Select Employee\'s role from the list below',
-      choices: ['Manager', 'Engineer', 'Intern']
+    // if epmloyee type === Manager
+    // ask questions + manager questions
+
+    // if employee type === Engineer
+    // ask questions + engineer questions
+
+    // if employee type === Intern
+    // ask questions + intern questions
+    type: "list",
+    name: "role",
+    message: "Select Employee's role from the list below",
+    choices: ["Manager", "Engineer", "Intern"],
   }
-  .then(response => {
-      console.log(response);
-      const data = employee(response);
-      writeToFile(data);
-  })
+  // then use responses to populate html cards
+  .then((response) => {
+    console.log(response);
+    const data = new Employee(response);
+    writeToFile(data);
+  }),
 ]);
 
-// if epmloyee type === Manager
-// ask questions + manager questions
 
-// if employee type === Engineer
-// ask questions + engineer questions
-
-// if employee type === Intern
-// ask questions + intern questions
-
-function writeToFile(data) {
-  fs.writeFile("../dist/index.html", data, function (err) {
+function writeHtmlFile(data) {
+  fs.writeHtmlFile("../dist/index.html", data, function (err) {
     if (err) {
       throw err;
     } else {
@@ -63,5 +62,66 @@ function writeToFile(data) {
 }
 
 
+// render HTML document with input responses
+function generateHtml(inputObj) {
+  console.log(inputObj);
 
-function generateHtml() {}
+  const htmlStr = `<!doctype html>
+  <html lang="en">
+  
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  
+      <link rel="stylesheet" href="./assets/styles.css">
+    <title>Hello, world!</title>
+  </head>
+  
+  <body>
+  
+    <div class="container">
+      <div class="jumbotron">
+        <h1 class="display-4 text-center">My Team</h1>
+  
+        <hr class="my-4">
+  
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card" style="max-width: 18rem;">
+              <div class="card-header text-white bg-dark">
+                Name<br>
+                Role
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID</li>
+                <li class="list-group-item">Email</li>
+                <li class="list-group-item">Other</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+  
+      </div>
+    </div>
+  
+  
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+      crossorigin="anonymous"></script>
+  
+    <script src="main.js"></script>
+  </body>
+  
+  </html>`;
+
+  writeHtmlFile(htmlStr);
+}
+
+generateHtml();
